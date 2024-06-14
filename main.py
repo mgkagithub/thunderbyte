5ecfa8648d4b80aa75a490692e3dd41d73f741dc
mgkagithub <omegarapid123456789@gmail.com>
2024-06-14 17:02:22 +0530

    massive update , hopefully it works

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import runpy
+import datetime
+
 while True:
     try:
         file_globals = runpy.run_path("gass.py")
     except Exception as error:
-        print(error)
-
-
-
-
-
-
-
-
-
+        print()
+        with open("error_logs.txt", "a") as e:
+            now = datetime.datetime.now()
+            error_message = f"{error} - {now.strftime('%I:%M %p, %d/%m/%Y, %A.')}\n"
+            e.write(error_message)
